9c902849fd8295b72238fe824a6d19c39a5a6887
maheshbhuvanagiri <Mahesh_Bhuvanagiri@epam.com>
2016-08-18 16:24:56 +0530

    FrontEnd libraries

---
+++ WebResources/Core/Framework/scripts/libs/core.spin.js
@@ -0,0 +1,97 @@
+﻿var core = core || {};
+(function () {
+
+    if (!$.fn.spin) {
+        return;
+    }
+
+    core.libs = core.libs || {};
+
+    core.libs.spinjs = {
+
+        spinner_config: {
+            lines: 11,
+            length: 0,
+            width: 10,
+            radius: 20,
+            corners: 1.0,
+            trail: 60,
+            speed: 1.2,
+            blockUI: true
+        },
+
+        //Config for busy indicator in element's inner element that has '.core-busy-indicator' class.
+        spinner_config_inner_busy_indicator: {
+            lines: 11,
+            length: 0,
+            width: 4,
+            radius: 7,
+            corners: 1.0,
+            trail: 60,
+            speed: 1.2,
+            blockUI: true
+        }
+
+    };
+
+    core.ui.setBusy = function (elm, optionsOrPromise) {
+        optionsOrPromise = optionsOrPromise || {};
+        //Check if it's promise
+        if (optionsOrPromise.always || optionsOrPromise['finally']) {
+            optionsOrPromise = {
+                promise: optionsOrPromise
+            };
+        }
+
+        var options = $.extend({}, core.libs.spinjs.spinner_config, optionsOrPromise);
+
+        if (!elm) {
+            if (options.blockUI) {
+                core.ui.block();
+            }
+            $('body').spin(options);
+        } else {
+            var $elm = $(elm);
+            //What if  more than one element. What if there are nested elements?
+            var $busyIndicator = $elm.find('.core-busy-indicator');
+            if ($busyIndicator.length) {
+                $busyIndicator.spin($.extend({}, core.libs.spinjs.spinner_config_inner_busy_indicator, optionsOrPromise));
+            } else {
+                if (options.blockUI) {
+                    core.ui.block(elm);
+                }
+                $elm.spin(options);
+            }
+        }
+        //Supports Q and jQuery.Deferred
+        if (options.promise) {
+            if (options.promise.always) {
+                options.promise.always(function () {
+                    core.ui.clearBusy(elm);
+                });
+            } else if (options.promise['finally']) {
+                options.promise['finally'](function () {
+                    core.ui.clearBusy(elm);
+                });
+            }
+        };
+    };
+
+    core.ui.clearBusy = function (elm) {
+        //Maybe better to do not call unblock if it's not blocked by setBusy
+        if (!elm) {
+            core.ui.unblock();
+            $('body').spin(false);
+        } else {
+            var $elm = $(elm);
+            var $busyIndicator = $elm.find('.core-busy-indicator');
+            if ($busyIndicator.length) {
+                $busyIndicator.spin(false);
+            } else {
+                core.ui.unblock(elm);
+                $elm.spin(false);
+            }
+        }
+    };
+
+})();
---
+++ WebResources/gruntfile.js
@@ -0,0 +1,67 @@
+﻿/*
+This file in the main entry point for defining grunt tasks and using grunt plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
+*/
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        //loading the Node modules
+        pkg: grunt.file.readJSON('package.json'),
+        bowercopy: {
+            options: {
+                // Bower components folder will be removed afterwards is set to true
+                clean: false
+            },
+            // Copying the Js scripts
+            scripts: {
+                options: {
+                    destPrefix: 'Core/Framework/scripts/bower'
+                },
+                files: {
+                    // Keys are destinations (prefixed with `options.destPrefix`)
+                    // Values are sources (prefixed with `options.srcPrefix`); One source per destination
+                    // e.g. 'bower_components/chai/lib/chai.js' will be copied to 'test/js/libs/chai.js'
+                    'angular/angular.js': 'angular/angular.js',
+                    'angular/angular.min.js': 'angular/angular.min.js',
+                    'angular/angular-ui-router.js': 'angular-ui-router/release/angular-ui-router.js',
+                    'angular/angular-ui-router.min.js': 'angular-ui-router/release/angular-ui-router.min.js',
+                    'angular/angular-sanitize.js': 'angular-sanitize/angular-sanitize.js',
+                    'angular/angular-sanitize.min.js': 'angular-sanitize/angular-sanitize.min.js',
+                    'angular/angular-sanitize.min.js.map': 'angular-sanitize/angular-sanitize.min.js.map',
+                    'jquery.js': 'jquery/jquery.js',
+                    'jquery.min.js': 'jquery/jquery.min.js',
+                    'jquery.min.map': 'jquery/jquery.min.map',
+                    'jquery.blockUI.js': 'blockui/jquery.blockUI.js',
+                    'bootstrap.js': 'bootstrap/dist/js/bootstrap.js',
+                    'bootstrap.min.js': 'bootstrap/dist/js/bootstrap.min.js',
+                    'spin.js': 'spin.js/spin.js',
+                    'spin.min.js': 'spin.js/spin.min.js',
+                    'jquery.spin.js': 'spin.js/jquery.spin.js',
+                    'stacktrace.js': 'stacktrace/dist/stacktrace.js',
+                    'stacktrace.min.js': 'stacktrace/dist/stacktrace.min.js',
+                    'stacktrace.js.map': 'stacktrace/dist/stacktrace.js.map',
+                    'toastr.js': 'toastr/toastr.js',
+                    'toastr.min.js': 'toastr/toastr.min.js',
+                    'toastr.js.map': 'toastr/toastr.js.map'
+                }
+            },
+            // Copying the Css Files
+            styles: {
+                options: {
+                    destPrefix: 'Core/Framework/vendorstyles/bower'
+                },
+                files: {
+                    'bootstrap.css': 'bootstrap/dist/css/bootstrap.css',
+                    'bootstrap.min.css': 'bootstrap/dist/css/bootstrap.min.css',
+                    'fonts': 'bootstrap/dist/fonts'
+                },
+            }
+        }
+    });
+
+    // Loading Dependent packages
+    grunt.loadNpmTasks('grunt-bowercopy');
+
+    //Registering the default tasks
+    grunt.registerTask('default', ['bowercopy']);
+};
